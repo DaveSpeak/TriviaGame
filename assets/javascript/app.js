14920762c90974537413d3df451911c0aeb940cf
DaveSpeak <DaveSpeak@gmail.com>
2016-08-03 23:07:17 -0700

    Final styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,13 +4,13 @@
 			info: {questAns:[
 				{question:"Who wrote Game of Thrones?",
 				 answer: ["Robert Heinlein", "George R.R. Martin", "Kathleen Kennedy",  "Peter Dinklage"],
-				 correct:1, picture:"George-RR-Martin.jpg", isAsked:false},
+				 correct:1, picture:"George-RR-Martin.gif", isAsked:false},
 				{question:"How does Tywin Lannister die?",
 				 answer: ["He doesn't", "He's beheaded by Joffrey", "Duel with Ned Stark",  "Shot with a crossbow"],
 				 correct:3, picture:"tyshot.gif", isAsked:false},
 				{question:"Who is Joffrey's father?",
 				 answer: ["Robert Baratheon", "Ned Stark", "Jamie Lannister",  "George-RR-Martin"],
-				 correct:2, picture:"Jamie-Lannister.jpg", isAsked:false},
+				 correct:2, picture:"Jamie-Lannister.gif", isAsked:false},
 				{question:"Which Game of Thrones actress was a bond girl?",
 				 answer: ["Diana Rigg", "Natalie Dormer", "Michelle Fairley",  "Emilia Clark"],
 				 correct:0, picture:"Diana-Rigg.gif", isAsked:false},
@@ -22,7 +22,7 @@
 				 correct:2, picture:"Tyrion-Lannister.gif", isAsked:false},
 				{question:"Who is Daenerys Targaryen's brother?",
 				 answer: ["Tywin", "Ned", "The Hound",  "Viserys"],
-				 correct:3, picture:"Viserys-Targeryen.jpg", isAsked:false},
+				 correct:3, picture:"Viserys.gif", isAsked:false},
 				{question:"Which character does Emilia Clark play?",
 				 answer: ["Ayra Stark", "Cersei Lannister", "Daenerys Targaryen",  "Yara Greyjoy"],
 				 correct:2, picture:"Daenerys.gif", isAsked:false}
@@ -34,13 +34,13 @@
 		q:-1,
 		waitaSecond:0,
 		time:0,
-		maxTime:10,
+		maxTime:30,
 		counter:0,
 		gameStart:function(){
 			var win = new Audio('assets/music/1470110884.mp3');
 			win.loop=true;
 			win.play();
-			var initial = "<button class='btn' id='selection'><h4>Start</h4></button>";
+			var initial=$('<button>').attr({'class':'btn','id':'selection'}).append('<h4>'+"Start");
 			$("#gameArea").html(initial);
 			$("#selection").on('click', triviaGame.selectQuestion);
 		},
@@ -56,19 +56,17 @@
 		$(".questionArea").empty();
 			triviaGame.time = 0;
 	        triviaGame.counter = setInterval(triviaGame.increment, 1000);
-			var triviaQuestion="<div class='questionArea'><br><h1>"+triviaGame.info.questAns[triviaGame.q].question+"</h1><br><ul>"+
-			"<li>"+"<button class='btn response' value=0><h3>"+triviaGame.info.questAns[triviaGame.q].answer[0]+"</h3></button></li>"+
-			"<li>"+"<button class='btn response' value=1><h3>"+triviaGame.info.questAns[triviaGame.q].answer[1]+"</h3></button></li>"+
-			"<li>"+"<button class='btn response' value=2><h3>"+triviaGame.info.questAns[triviaGame.q].answer[2]+"</h3></button></li>"+
-			"<li>"+"<button class='btn response' value=3><h3>"+triviaGame.info.questAns[triviaGame.q].answer[3]+"</h3></button></li>"+
-			"</ul></div>"
-			$("#gameArea").html(triviaQuestion);
-			var timeDisplay="<div id='timeRem'><h1>Time Remaining: "+(triviaGame.maxTime-triviaGame.time)+" seconds</h1></div>";
+			var timeDisplay=$('<div>').attr('id','timeRem').append('<h1>'+"Time Remaining: "+(triviaGame.maxTime-triviaGame.time)+" seconds");
 	        $('#time').html(timeDisplay);
+	        var triviaQuestion=$('<div>').attr('class','questionArea').append('<br>','<h1>'+triviaGame.info.questAns[triviaGame.q].question,'<br>');
+	        for (i=0;i<4;i++){
+	        	triviaQuestion.append($('<button>').attr({'class':'btn response','value':i}).append('<h3>'+
+	        	triviaGame.info.questAns[triviaGame.q].answer[i]));
+	        }
+			$("#gameArea").html(triviaQuestion);
 			triviaGame.info.questAns[triviaGame.q].isAsked=true;
 			$("button").on('click',function(){
 				var i=$(this).attr('value');
-				console.log(i);
 				if (i==triviaGame.info.questAns[triviaGame.q].correct){
 					triviaGame.correct++;
 					triviaGame.displayCorrect();
@@ -81,10 +79,10 @@
 	 	increment:function(){
 			triviaGame.time++;
 			if (triviaGame.maxTime-triviaGame.time < 10){
-				var timeDisplay="<div id='timeRem'><h1>Time Remaining: &nbsp"+(triviaGame.maxTime-triviaGame.time)+" seconds</h1></div>";
+				var timeDisplay=$('<div>').attr('id','timeRem').append('<h1>'+"Time Remaining: &nbsp"+(triviaGame.maxTime-triviaGame.time)+" seconds");
 
 			}else {
-				var timeDisplay="<div id='timeRem'><h1>Time Remaining: "+(triviaGame.maxTime-triviaGame.time)+" seconds</h1></div>";
+				var timeDisplay=$('<div>').attr('id','timeRem').append('<h1>'+"Time Remaining: "+(triviaGame.maxTime-triviaGame.time)+" seconds");
 			}
 			$('#time').empty();
 	        $('#time').html(timeDisplay);
@@ -97,34 +95,44 @@
 			clearTimeout(triviaGame.waitaSecond);
 			clearInterval(triviaGame.counter);
 			$(".questionArea").empty();
-			var winner='<div class="questionArea"><br><h1>Correct!!!</h1><br><img src="assets/images/'+triviaGame.info.questAns[triviaGame.q].picture+'"></div>';
+			var winner=$('<div>');
+			winner.addClass('questionArea');
+			winner.append('<br>','<h1>'+"Correct!!!",'<br>',$('<img>').attr('src','assets/images/'+
+				triviaGame.info.questAns[triviaGame.q].picture));
 			$('#gameArea').html(winner);
-			triviaGame.waitaSecond = setTimeout(triviaGame.selectQuestion, 3000);
+			triviaGame.waitaSecond = setTimeout(triviaGame.selectQuestion, 5000);
 			return;
 		},
 		displayWrong:function(){
 			clearTimeout(triviaGame.waitaSecond);
 			clearInterval(triviaGame.counter);
 			$(".questionArea").empty();
-			var loser='<div class="questionArea"><br><h1>Sorry!!</h1><br><h1>The correct answer is: '+
-			triviaGame.info.questAns[triviaGame.q].answer[triviaGame.info.questAns[triviaGame.q].correct]+'</h1><br><img src="assets/images/'+triviaGame.info.questAns[triviaGame.q].picture+'"></div>';
+			var loser=$('<div>');
+			loser.addClass('questionArea');
+			loser.append('<br>','<h1>'+"Sorry!!",'<br>','<h1>'+"The correct answer is: "+
+			triviaGame.info.questAns[triviaGame.q].answer[triviaGame.info.questAns[triviaGame.q].correct],'<br>',
+			$('<img>').attr('src','assets/images/'+triviaGame.info.questAns[triviaGame.q].picture));
 			$('#gameArea').html(loser);
-			triviaGame.waitaSecond = setTimeout(triviaGame.selectQuestion, 3000);
+			triviaGame.waitaSecond = setTimeout(triviaGame.selectQuestion, 5000);
 			return;
 		},
 		displayUnanswered:function(){
 			$(".questionArea").empty();
-			var unans='<idv class="questionArea"><br><h1>Time\'s up!!!</h1><br><h1>The correct answer is: '+
-			triviaGame.info.questAns[triviaGame.q].answer[triviaGame.info.questAns[triviaGame.q].correct]+'</h1><br><img src="assets/images/'+triviaGame.info.questAns[triviaGame.q].picture+'"></div>';
+			var unans=$('<div>');
+			unans.addClass('questionArea');
+			unans.append('<br>','<h1>'+"Time\'s up!!!",'<br>','<h1>'+"The correct answer is: "+
+			triviaGame.info.questAns[triviaGame.q].answer[triviaGame.info.questAns[triviaGame.q].correct],
+			$('<img>').attr('src','assets/images/'+triviaGame.info.questAns[triviaGame.q].picture));
 			$('#gameArea').html(unans);
 			triviaGame.unanswered++;
-			triviaGame.waitaSecond = setTimeout(triviaGame.selectQuestion, 3000);
+			triviaGame.waitaSecond = setTimeout(triviaGame.selectQuestion, 5000);
 			return;
 		},
 		finishGame:function(){
 			$(".questionArea").empty();
-			var complete ='<div id="results"><br><h1>Game Over!!</h1><br><h1>Correct: '+triviaGame.correct+
-			'</h1><br><h1>Wrong: '+triviaGame.wrong+'</h1><br><h1>Unanswered: '+triviaGame.unanswered+'</h1><br><br><button class="btn" id="restart">Restart</button></div>';
+			var complete=$('<div>').attr('id','results').append('<br>','<h1>'+"Game Over!!",'<br>','<h1>'+"Correct: "+
+			triviaGame.correct,'<br>','<h1>'+"Wrong: "+triviaGame.wrong,'<br>','<h1>'+"Unanswered: "+
+			triviaGame.unanswered,'<br>','<br>',$('<button>').attr({'class':'btn','id':'restart'}).append("Restart"));
 			$('#gameArea').html(complete);
 			triviaGame.correct=0;
 			triviaGame.wrong=0;
